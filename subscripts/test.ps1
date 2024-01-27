6b9920f0dbd69d9b5d02bb1916ed7b84e5215689
Paramvir Das <paramvir2307@gmail.com>
2024-01-27 21:39:26 +0100

    remove test.ps1

--- subscripts/test.ps1
+++
@@ -1,7 +0,0 @@
-$git_status = git status
-if ($git_status.Length -gt 0) {
-    $match = $git_status[0] | Select-String -Pattern '^On branch (.*)$'
-    $branch_name = $match.Matches.Groups[1].Value
-}
-
-Write-Host $branch_name
