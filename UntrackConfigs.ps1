ac395f708d4bb4c3491648b9bec682a4b88f9765
Bob <mymailaddress@mailprovider.domainsuffix>
2024-08-20 19:04:39 +0200

    include psd file to untrack config list due to changing date

--- UntrackConfigs.ps1
+++ UntrackConfigs.ps1
@@ -1,2 +1,3 @@
 git update-index --skip-worktree $PSScriptRoot\config\config.yml
 git update-index --skip-worktree $PSScriptRoot\config\PersonalProfileConfig.ps1
+git update-index --skip-worktree $PSScriptRoot\ScriptboxUtils\ScriptboxUtils.psd1
